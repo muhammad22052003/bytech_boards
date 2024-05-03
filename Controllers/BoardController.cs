2b333502141c838ea3e3e8f0f1788da90f4f7519
Muhammad <muhammadarch22@gmail.com>
2024-05-03 18:00:20 +0500

    path edit

--- Controllers/BoardController.cs
+++ Controllers/BoardController.cs
@@ -50,7 +50,7 @@ namespace bytech_boards.Controllers
             };
 
 
-            board.PathToFile = "wwwroot/data/" + board.Id.ToString().Replace("-", "") + ".bmp";
+            board.PathToFile = board.Id.ToString().Replace("-", "") + ".bmp";
 
             await _dBService.AddData("boards", board);
 
